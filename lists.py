d53b7dcb8eb3fa1edb1f6592e3a2f403b8f149fe
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-01-06 20:32:00 -0800

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,13 @@
+#lists & title()
+
+bikes=['trek','giant','specialized','canyan']
+print(bikes[3])
+print(bikes[3].upper())
+print(bikes)
+bikes[0] = "generic"
+print(bikes)
+bikes.append('trek')
+print(bikes)
+
+#to run in CL; E:\python_crash> python lists.py
+
