ac4a2d17865a79810e2bc0cbf7fda3052d1ba347
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-01-11 13:23:12 -0800

    len() min() max() sum()

--- lists4.py
+++ lists4.py
@@ -1,3 +1,9 @@
 cars=['volvo', 'toyota', 'bmw', 'benz', 'audi', 'aston martin']
-l=len(cars)
+length=len(cars)
+print(length)
+
+l = [i for i in range(-1,-20,-2)]
 print(l)
+print(min(l))
+print(max(l))
+print(sum(l))
