0720c8e3c06ba361bea7678beaf8e75017e22e58
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-01-12 21:48:02 -0800

    easily mistaken details around structures

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+#dictionaries
+#use ":" NOT "=" for key-value pairing
+
+robot1 = {'fraction': 'police', 'weapon': 'uzi', 'x-axis': 4, 'y-axis': 5}
+robot2 = {'fraction': 'terrorist', 'weapon': 'ak47', 'x-axis': 2, 'y-axis': 25}
+print(robot1)
+print(robot2['weapon'])
+#addition; use squre bracket [] and an "=" assignment
+#this addition is really easy to forget
+robot1['HP'] = 100
+print(robot1)
