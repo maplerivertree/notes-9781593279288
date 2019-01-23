e7304b073efd5629d61798adaedae0e6c233c5d7
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-01-22 21:40:55 -0800

    positional argument, keyword argument, default values

--- function2.py
+++ function2.py
@@ -1,4 +1,4 @@
-#positional argument, keyword argument
+#positional argument, keyword argument, default values
 
 def make_a_point(a, b):
 	return(a + ' catch ' + b +".")
@@ -9,3 +9,11 @@ print(make_a_point('mice', 'cats'))
 
 #keyword arguments
 print(make_a_point(b = 'mice', a = 'cats'))
+
+
+#default values
+def the_hunt(a, b = 'the hunted'):
+	return(a + ' hunts ' + b + ".")
+
+
+print(the_hunt('lions'))
