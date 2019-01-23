25dbd5c582abd9bb60ff9b9320ab926d9b899b94
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-01-22 18:54:08 -0800

    Add files via upload

---
+++ Flag.py
@@ -0,0 +1,34 @@
+
+print("Game Rule: You need to type in the result of 1x27, 2x27, 3x27, and etc.")
+Print("You have 3 chances to restart")
+
+count = 1
+changes = 3
+Game_on = True
+user_input_his = [-2,-1,0]
+
+while Game_on:
+	while changes >0:
+
+
+
+
+		user_input=input('Type in your number: ')
+		
+
+
+
+		if count = 1:
+			count += 1
+			continue
+
+		user_input_his.append(int(user_input))
+		if user_input_his[-1] <= user_input_his[-2]:
+			print("incorrect answer 1, please re-enter.")
+
+
+
+
+while true
+break
+continue
