c903e67fc0e206fb8d3a7ede353620630f0ad963
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-02-03 11:50:18 -0800

    # self prefix, Method_name(self), use method to change attri

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 #Classes
-# self prefix, Method(self)
+# self prefix, Method_name(self), use method to change attri
 
 class occupation():
 	"""docstring for ClassName"""
@@ -13,11 +13,20 @@ class occupation():
 	def start_time(self, t1):
 		print('Your work starts at '+t1+' in the morning.')
 
+	def change_job(self, new_job):
+		print('My current job is a ' + str(self.name) + ' and my new job is the below: ')
+		self.name = new_job
+		print(str(self.name))
 
 
 my_job = occupation('PM', 10, '150K')
 my_job_name = my_job.name
+
 print(my_job_name)
+print('\n')
+
+my_new_job = my_job.change_job('golfer')
+
 
 my_job.start_time('9:30')
 
